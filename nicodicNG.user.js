ae7b596a46fa2cb3e38051d700887c1e1d112b99
yakineko <you74674@gmail.com>
2017-05-21 22:28:42 +0800

    add NG button next to each ID

--- nicodicNG.user.js
+++ nicodicNG.user.js
@@ -9,18 +9,17 @@
 // @include		https://dic.nicomoba.jp/k/b/a/*
 // @include		http://dic.nicovideo.jp/t/b/a/*
 // @include		https://dic.nicovideo.jp/t/b/a/*
-// @version		1.6.2
+// @version		1.7
 // @grant		none
 // @description	ニコニコ大百科掲示板NG機能。IDを入力して設定を押せばNGできます。
 // ==/UserScript==
 
 var url = window.location.href;
 
-
-var doNGImpl;
 var doNG;
+var addNGButton;
 if(url.indexOf("dic.nicovideo.jp/t/b/a")===-1){//PC or nicomoba
-	doNGImpl=function doNGImpl(dl, ngList){
+	var doNGImpl=function doNGImpl(dl, ngList){
 		var resheads=dl.getElementsByTagName("dt");
 		var resbodies=dl.getElementsByTagName("dd");
 		for(var i=0; i<resheads.length; i++){
@@ -29,22 +28,39 @@ if(url.indexOf("dic.nicovideo.jp/t/b/a")===-1){//PC or nicomoba
 			for(var j=0; j<ngList.length; j++){
 				if(reshead.textContent.indexOf(ngList[j])!=-1){
 					reshead.childNodes[3].textContent="NGしました";
-					reshead.childNodes[4].textContent=" ：NGしました ID: "+ngList[j];
+					reshead.childNodes[4].textContent=" ：NGしました ID: "+ngList[j]+" ";
 					resbody.textContent="NGしました";
 				}
 			}
 		}
 	};
+	var getBBS=function(){
+		return document.getElementsByTagName("dl");
+	};
 	doNG=function doNG(NGList){
 		console.log("doNG PC or nicomoba");
 		var ngList=NGList.value.split('\n').filter(function(el) {return el.length !== 0;});
-		var bbs = document.getElementsByTagName("dl");
+		var bbs = getBBS();
 		for(var dl=0; dl<bbs.length; dl++)
 			doNGImpl(bbs[dl], ngList);
 	};
+	
+	addNGButton=function addNGButton(NGList){
+		$('dt').append("<NG style=\"cursor: pointer; color:blue;\">NG</NG>");
+		$('NG').click(function(e){
+			var NG=e.target;
+			var start=NG.previousSibling.textContent.indexOf("ID:")+4;
+			var ID=NG.previousSibling.textContent.substr(start, 10);
+			if(NGList.value.indexOf(ID)==-1)
+			{
+				NGList.value+="\n"+ID;
+				doNG(NGList);
+			}
+		});
+	};
 }
 else{//mobile
-	doNGImpl=function doNGImpl(bbs, ngList){
+	var doNGImpl=function doNGImpl(bbs, ngList){
 		var posts=bbs.getElementsByTagName("p");
 		for(var i=0; i<posts.length; i++){
 			var post=posts[i];
@@ -60,10 +76,13 @@ else{//mobile
 			}
 		}
 	};
+	var getBBS=function(){
+		return document.getElementById("bbs_wrapper");
+	};
 	doNG=function doNG(NGList){
 		console.log("doNG mobile");
 		var ngList=NGList.value.split('\n').filter(function(el) {return el.length !== 0;});
-		var bbs = document.getElementById("bbs_wrapper");
+		var bbs = getBBS();
 		doNGImpl(bbs, ngList);
 	};
 }
@@ -92,6 +111,7 @@ function getNGdiv(){
 	div.appendChild(NGButton);
 
 	div.NGList=NGList;
+	
 	return div;
 }
 
@@ -110,6 +130,7 @@ function getNGdiv(){
 	else if(url.indexOf("nicomoba")===-1){//PC
 		div = document.getElementById("main");
 		div.appendChild(NGdiv);
+		addNGButton(NGdiv.NGList);
 	}
 	else{//nicomoba
 		div = document.querySelectorAll("[id='bottom']")[1];
